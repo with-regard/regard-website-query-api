ac6d7e7b6be1a547f43c9b0fb4677c86ba42a01c
David Blurton <david.blurton@gmail.com>
2014-07-14 10:40:56 +0100

    Get organization and project from the url

--- controllers/registerQuery.js
+++ controllers/registerQuery.js
@@ -4,11 +4,14 @@ var Investigation = require('../schemas/investigation.js');
 
 var router = express.Router();
 
-router.post('/register', function (req, res, next) {
+router.post(':organization/:project/register', function (req, res, next) {
+  var organization = req.params.organization;
+  var project = req.params.project;
+
   var queryName = req.body.query.id;
   var queryDefinition = req.body.query.queryDefinition;
 
-  var dataStore = new DataStore(req.body.investigation.organization, req.body.investigation.product);
+  var dataStore = new DataStore(organization, project);
 
   dataStore.registerQuery(queryName, queryDefinition).done(function () {
     res.send(201); // created
